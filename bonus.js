5b6b70f1d77601a41e0170825543855146049d63
Alex Capon <caplex.work@gmail.com>
2025-02-18 19:59:10 +0700

    hw done!

--- bonus.js
+++ bonus.js
@@ -1,6 +1,6 @@
 // 1) Создать приватное поле в функции-конструкторе, создать геттер и сеттер для него.
 class SomeClass {
-  constructor() {}
+  constructor() { }
   #privateField = "It's private!"
 
   getPrivate() { return this.#privateField; };
@@ -19,5 +19,13 @@ arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 total = 13;
 
 function getSumPair(arr, target) {
-  
+  const map = new Map();
+
+  for (let i = 0; i < arr.length; i+=1) {
+    const diff = target - arr[i];
+    if(map.has(diff)) return [map.get(diff), i];
+    map.set(arr[i], i);
+  }
 }
+
+console.log(getSumPair(arr, total));
